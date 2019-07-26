44bd5c4dc11077c3c8cf68f0c81faf5c9965c4fa
Mochamad Arifin <klampok.child@gmail.com>
2019-07-26 20:23:14 +0700

    Deploy listen Globally for Direct API

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ yarn build
 echo "Uplading changes..."
 rsync -rav ./dist/index.js "klampok:/home/pilkades/server"
 echo "Restarting server..."
-ssh klampok "sh -c 'killall node; cd /home/pilkades/server; nohup node index.js > /dev/null 2>&1 &'"
+ssh klampok "sh -c 'killall node; cd /home/pilkades/server; nohup node index.js --host 0.0.0.0 > /dev/null 2>&1 &'"
 
